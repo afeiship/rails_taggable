b04c2ea2240d8f7e2530b1a316f76166adb60bd9
afeiship <1290657123@qq.com>
2021-12-18 09:51:22 +0800

    fix: rm class_option

--- lib/generators/rails_taggable/install_generator.rb
+++ lib/generators/rails_taggable/install_generator.rb
@@ -1,7 +1,6 @@
 module RailsTaggable
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("templates", __dir__)
-    class_option :full, :type => :boolean
     desc "Configure necessary files to use RailsTaggable"
 
     def create_migration
